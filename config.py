c6171517aeb8e13c2d02e686d62f4556c472dd17
Oscarhou1 <1houosc@hdsb.ca>
2026-01-12 23:14:14 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+# ---------------- settings ----------------
+W, H = 800, 600
+FPS = 60
+
+WHITE = (255,255,255)
+RED   = (255,80,80)
+BLUE  = (80,80,255)
+BLACK = (0,0,0)
+
+# ---------------- assets (put them in same folder) ----------------
+# I didn't make fancy loading functions here — loading straight up
+BG_FILE    = "backgroundmy.jpg"
+TOWER_FILE = "gun tower.png"
+BULLET_FILE = "bulllet.png"
+MUSIC_FILE = "game-music-loop-7-145285 (1).mp3"
+GUN_SOUND  = "gunfire.mp3"
